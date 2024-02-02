5924c75ce9f973f6513edcccd06e0fa0cfa52e23
yonilhp <yonil.hinostroza.27@unsch.edu.pe>
2024-02-02 14:22:07 -0500

    Sin errores en el cliente, terminé por completo la sección 11

--- lib/src/pages/client/products/list/client_products_list_page.dart
+++ lib/src/pages/client/products/list/client_products_list_page.dart
@@ -1,4 +1,8 @@
 import 'package:apu_market/src/pages/client/products/list/client_products_list_controller.dart';
+import 'package:apu_market/src/pages/delivery/orders/list/delivery_orders_list_page.dart';
+import 'package:apu_market/src/pages/register/register_page.dart';
+import 'package:apu_market/src/pages/restaurant/orders/list/restaurant_orders_list_page.dart';
+import 'package:apu_market/src/utils/custom_animated_bottom_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -8,24 +12,50 @@ class ClientProductsListPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text('ClientProductsListPage'),
-          ElevatedButton(
-            onPressed: () {
-              con.signOut();
-            },
-            child: Text(
-              'Cerrar Sesión',
-              style: TextStyle(
-                color: Colors.black,
-              ),
+        bottomNavigationBar: _bottomBar(),
+        body: Obx(() => IndexedStack(
+              index: con.indexTab.value,
+              children: [
+                RestaurantOrdersListPage(),
+                DeliveryOrdersListPage(),
+                RegisterPage()
+              ],
+            )));
+  }
+
+  Widget _bottomBar() {
+    return Obx(() => CustomAnimatedBottomBar(
+          //color de bordes de barra
+
+          containerHeight: 70,
+          backgroundColor: Colors.amber,
+          //color de bordes de bottomBar
+          // backgroundColor: Color.fromARGB(255, 255, 248, 220),
+          showElevation: true,
+          itemCornerRadius: 24,
+          curve: Curves.easeIn,
+          selectedIndex: con.indexTab.value,
+          onItemSelected: (index) => con.changeTab(index),
+          items: [
+            BottomNavyBarItem(
+              icon: Icon(Icons.apps),
+              title: Text('Home'),
+              activeColor: Colors.white,
+              inactiveColor: Colors.black,
             ),
-          )
-        ],
-      ),
-    ));
+            BottomNavyBarItem(
+              icon: Icon(Icons.list),
+              title: Text('Mis pedidos'),
+              activeColor: Colors.white,
+              inactiveColor: Colors.black,
+            ),
+            BottomNavyBarItem(
+              icon: Icon(Icons.person),
+              title: Text('Perfil'),
+              activeColor: Colors.white,
+              inactiveColor: Colors.black,
+            ),
+          ],
+        ));
   }
 }
